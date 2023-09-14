f122f6d364a669a07fc30885b9ba75437f3d39dc
Amagash <tiffanysouterre@gmail.com>
2023-09-14 16:33:31 -0700

    get html page

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-from bs4 import BeautifulSoup
 import requests 
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import json
 from urllib.request import urlopen
-url = "https://aws.amazon.com/tutorials/create-banking-bot-on-amazon-lex-v2-console/" 
 
-page = urlopen(url)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
-print(soup.get_text().replace("\n", " "))
+def getdata(url):
+    page = requests.get(url)
+    return page.text
+
+
+# page = urlopen(url)
+# html = page.read().decode("utf-8")
+# soup = BeautifulSoup(html, "html.parser")
+# print(soup.get_text().replace("\n", " "))
  
-# payload = { 
-# 	"uname": "test", 
-# 	"pass": "test" 
-# } 
-# s = requests.session() 
-# response = s.post(URL, data=payload) 
-# print(response.status_code) # If the request went Ok we usually get a 200 status. 
- 
-# from bs4 import BeautifulSoup 
-# soup = BeautifulSoup(response.content, "html.parser") 
-# protected_content = soup.find(attrs={"id": "pageName"}).text 
-# print(protected_content)
+if __name__ == "__main__":
+
+    url = "https://aws.amazon.com/tutorials/create-banking-bot-on-amazon-lex-v2-console/" 
+    print(getdata(url))
