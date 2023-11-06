4bd70757442a02730f6b67d211a8412fd9c66c5d
Amagash <tiffanysouterre@gmail.com>
2023-11-06 15:23:49 +0100

    update main

--- main.py
+++ main.py
@@ -2,47 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import json
-from urllib.request import urlopen
+import re
+
 
 def get_data(url):
-    page = requests.get(url)
-    return page.text
+    data = []
+    raw_data = requests.get(url).json()
+    i=1
+    for item in raw_data["items"][:3]:
+        product_url = item["item"]["additionalFields"]["productUrl"]
+        product_name = item["item"]["name"]
+        product_faq = product_url.replace('?did=ap_card&trk=ap_card', 'faqs')
+        f_product = open(f"{product_name}.html", "w")
+        f_product.write(requests.get(product_url).text)
+        f_product.close()
+        f_faq = open(f"{product_name}_faq.html", "w")
+        f_faq.write(requests.get(product_faq).text)
+        f_faq.close()
 
-def get_links(url):
-    raw_html = get_data(url)
-    soup = BeautifulSoup(raw_html, 'html.parser')
-    # links = [link.get('href') for link in soup.find_all('a', href=True)]
-    # print(links)
-    list_links = []
-    for link in soup.find_all('div', class_='m-hd'):
-        print(link)
-        # # Append to list if new link contains original link
-        # if str(link["href"]).startswith((str(url))):
-        #     list_links.append(link["href"])
-            
-        # # Include all href that do not start with website link but with "/"
-        # if str(link["href"]).startswith("/"):
-        #     if link["href"] not in dict_href_links:
-        #         print(link["href"])
-        #         dict_href_links[link["href"]] = None
-        #         link_with_www = url + link["href"][1:]
-        #         print("adjusted link =", link_with_www)
-        #         list_links.append(link_with_www)
-                
-    # Convert list of links to dictionary and define keys as the links and the values as "Not-checked"
-    # dict_links = dict.fromkeys(list_links, "Not-checked")
-    # print(dict_links)
+        i+=1
+        data.append({"product_name": product_name, "product_url": product_url, "product_faq": product_faq})
+    print (data)
+    return data
 
 
-
-# page = urlopen(url)
-# html = page.read().decode("utf-8")
-# soup = BeautifulSoup(html, "html.parser")
-# print(soup.get_text().replace("\n", " "))
- 
 if __name__ == "__main__":
 
-    url = "https://aws.amazon.com/developer/hands-on" 
-    # dict_href_links = {}
-    # get_links(url)
-    print(get_links(url))
+    url = "https://aws.amazon.com/api/dirs/items/search?item.directoryId=aws-products&sort_by=item.additionalFields.productNameLowercase&sort_order=asc&size=300&item.locale=en_US&tags.id=aws-products%23type%23service&tags.id=!aws-products%23type%23variant"
+    get_data(url)
